7cee7de77b3e9dc9209b729ff940fcf302fc28c6
mok-gi-tae <mokgitae@gmail.com>
2026-07-23 17:35:52 +0900

    feat: add RegionController

---
+++ backend/src/main/java/com/gitae/jpgourmetmap/controller/RegionController.java
@@ -0,0 +1,13 @@
+package com.gitae.jpgourmetmap.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/regions")
+@RequiredArgsConstructor
+public class RegionController {
+
+
+}
