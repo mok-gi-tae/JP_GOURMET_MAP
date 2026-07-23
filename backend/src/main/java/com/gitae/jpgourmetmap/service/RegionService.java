0afdfe171f8b30e471cca3cef1e0e32e8f84d7bb
mok-gi-tae <mokgitae@gmail.com>
2026-07-23 17:50:22 +0900

    feat add RegionService class

---
+++ backend/src/main/java/com/gitae/jpgourmetmap/service/RegionService.java
@@ -0,0 +1,25 @@
+package com.gitae.jpgourmetmap.service;
+
+import com.gitae.jpgourmetmap.dto.region.RegionResponse;
+import com.gitae.jpgourmetmap.repository.RegionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class RegionService {
+
+    private final RegionRepository regionRepository;
+
+    public List<RegionResponse> getRegions() {
+        return regionRepository.findAll()
+                .stream()
+                .map(RegionResponse::from)
+                .toList();
+    }
+
+}
