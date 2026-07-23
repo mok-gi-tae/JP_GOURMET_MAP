e44fcac74e198e4d9f43f8b93011ba421cc162d9
mok-gi-tae <mokgitae@gmail.com>
2026-07-23 18:02:26 +0900

    feat: add RegionController logic

--- backend/src/main/java/com/gitae/jpgourmetmap/controller/RegionController.java
+++ backend/src/main/java/com/gitae/jpgourmetmap/controller/RegionController.java
@@ -1,13 +1,25 @@
 package com.gitae.jpgourmetmap.controller;
 
+import com.gitae.jpgourmetmap.domain.Region;
+import com.gitae.jpgourmetmap.dto.region.RegionResponse;
+import com.gitae.jpgourmetmap.service.RegionService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("api/regions")
+@RequestMapping("/api/regions")
 @RequiredArgsConstructor
 public class RegionController {
 
+    private final RegionService regionService;
+
+    @GetMapping
+    public List<RegionResponse> getRegions() {
+        return regionService.getRegions();
+    }
 
 }
