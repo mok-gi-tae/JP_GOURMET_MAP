aa4eb6c2f17c23360ac93438c776a11ac191cb4e
mok-gi-tae <mokgitae@gmail.com>
2026-07-23 10:35:04 +0900

    feat: add Restaurant Repository and RegionRepository

---
+++ backend/src/main/java/com/gitae/jpgourmetmap/repository/RestaurantRepository.java
@@ -0,0 +1,9 @@
+package com.gitae.jpgourmetmap.repository;
+
+import com.gitae.jpgourmetmap.domain.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+
+
+}
