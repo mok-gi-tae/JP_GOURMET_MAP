d6f362b87dbf49b9412fb0b0f1929807ea09dedb
목기태 <xorlahr@gmail.com>
2026-07-26 23:17:57 +0900

    feat: add SignUpRequest and LoginRequest

---
+++ backend/src/main/java/com/gitae/jpgourmetmap/domain/user/SignUpRequest.java
@@ -0,0 +1,11 @@
+package com.gitae.jpgourmetmap.domain.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record SignUpRequest(
+        @NotBlank @Email String email,
+        @NotBlank @Size(min = 4, max = 20) String password,
+        @NotBlank String nickname
+) { }
