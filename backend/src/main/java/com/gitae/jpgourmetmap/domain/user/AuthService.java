03e458f971db2ec56c0c6a2a89dff467f718bb15
목기태 <xorlahr@gmail.com>
2026-07-26 23:44:19 +0900

    feat: add authService

--- backend/src/main/java/com/gitae/jpgourmetmap/domain/user/AuthService.java
+++ backend/src/main/java/com/gitae/jpgourmetmap/domain/user/AuthService.java
@@ -1,41 +1,48 @@
 package com.gitae.jpgourmetmap.domain.user;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
 
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class AuthService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    // 이메일 닉네임 을 중복체크
-    @Transactional
-
-    public void emailCheck (SignUpRequest signUpRequest) {
-        if (userRepository.existsByEmail(signUpRequest.email())) {
+    // 회원가입
+    public UserResponse signUp(SignUpRequest request) {
+        // 이메일 중복 체크
+        if (userRepository.existsByEmail(request.email())) {
             throw new IllegalArgumentException("이미 가입된 이메일입니다.");
         }
-    }
-
-    @Transactional
-    public void nicknameCheck (SignUpRequest signUpRequest) {
-        if (userRepository.existsByNickname(signUpRequest.nickname())) {
+        // 닉네임 중복 체크
+        if (userRepository.existsByNickname(request.nickname())) {
             throw new IllegalArgumentException("이미 존재하는 닉네임입니다.");
         }
+        // 회원가입 시 비밀번호 인코딩
+        User user = new User(
+                request.email(),
+                passwordEncoder.encode(request.password()),
+                request.nickname()
+        );
+        return UserResponse.from(userRepository.save(user));
     }
 
-    // 비밀번호 인코딩
-    // user 테이블에 저장
+    // 로그인
+    public UserResponse signIn(LoginRequest request) {
+        User user = userRepository.findByEmail(request.email())
+                .orElseThrow(() -> new IllegalArgumentException("이메일 틀림 ㅋ"));
 
-
+        if (!passwordEncoder.matches(request.password(), user.getPassword())) {
+            throw new IllegalArgumentException("비밀번호 틀림 ㅋ");
+        }
+        return UserResponse.from(user);
+    }
 
 }
