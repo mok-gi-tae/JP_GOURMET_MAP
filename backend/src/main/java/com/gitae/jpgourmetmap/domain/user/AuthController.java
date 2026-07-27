89d42e7ceb112bb853bfd10e89d94c34e7d92a64
mok-gi-tae <mokgitae@gmail.com>
2026-07-27 11:47:31 +0900

    fix: AuthController field

--- backend/src/main/java/com/gitae/jpgourmetmap/domain/user/AuthController.java
+++ backend/src/main/java/com/gitae/jpgourmetmap/domain/user/AuthController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
-    private AuthService authService;
+    private final AuthService authService;
 
     // 회원가입 페이지
     @PostMapping("/signup")
